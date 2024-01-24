8f11c92665447b3a360e27b46a0aef1b00a084d9
rkang <ryan.kang@nautilusdb.com>
2024-01-24 15:17:29 -0800

    test

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,17 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
+      <button><a href="/about">About chatbees</a></button>
+
       <Head>
         <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://www.chatbees.ai/logo_favicon.svg" />
       </Head>
 
       <main>
         <Header title="Welcome to chatbees + netlify integration demo!"/>
         <p className="Start chatting with chatbees by interacting with the ">
-          Get started by editing <code>pages/index.js</code>
+           This chatbot is trained on chatbees documentations. You can ask questions like "what is chatbees?"
         </p>
       </main>
 
