5bce6b26e1bd7934bab10285c710731bc0886d6d
rkang <ryan.kang@nautilusdb.com>
2024-01-31 13:47:15 -0800

    test

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,13 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to Netlify + ChatBees demo" />
+        <md-block>
+        #ChatBees Serverless Chat Platform for your knowledge base
+            
+        ##About
+        Welcome to ChatBees, AI Powered Chat Platform for your internal knowledge base! ChatBees helps you easily build an LLM application with your internal knowledge base. ChatBees summarizes information and gets insights through automated analysis of all your data. This helps users get a comprehensive understanding of either a single or all their data sources, including files, websites, Google Docs, Notion, etc.
 
+        </md-block>
       </main>
 
       <Footer />
